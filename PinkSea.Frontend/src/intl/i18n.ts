c1d7f13b9f9d5a7d5ada8f2b5a2a9fa5e662cca3
Naufal Alauddin Hilmi <naufal.alauddin@ui.ac.id>
2024-11-12 16:16:50 +0700

    feat: add Indonesian translation

--- PinkSea.Frontend/src/intl/i18n.ts
+++ PinkSea.Frontend/src/intl/i18n.ts
@@ -1,9 +1,13 @@
 import En from '@/intl/translations/en'
+import Id from '@/intl/translations/id'
 import Pl from '@/intl/translations/pl'
 
 export default {
   en: {
     translation: En
+  },
+  id: {
+    translation: Id
   },
   pl: {
     translation: Pl
---
+++ PinkSea.Frontend/src/intl/translations/id.ts
@@ -0,0 +1,49 @@
+export default {
+  sidebar: {
+    title: "PinkSea",
+    tag: "oekaki BBS",
+    shinolabs: "proyek shinonome laboratories"
+  },
+  menu: {
+    greeting: "Hai @{{name}}!",
+    invitation: "Masuk untuk mulai menggambar!",
+    input_placeholder: "@handle.keren.mu",
+    atp_login: "@ Masuk",
+    my_oekaki: "Oekaki-ku",
+    recent: "Terkini",
+    settings: "Pengaturan",
+    logout: "Keluar",
+    create_something: "Mulai Menggambar"
+  },
+  breadcrumb: {
+    recent: "terkini",
+    painter: "media gambar",
+    settings: "pengaturanmu",
+    user_profile: "profil {{handle}}",
+    user_post: "postingan {{handle}}",
+    tagged: "postingan dengan tag #{{tag}}"
+  },
+  timeline: {
+    by_before_handle: "Oleh ",
+    by_after_handle: "",
+  },
+  post: {
+    response_from_before_handle: "Balasan dari ",
+    response_from_after_handle: "",
+    response_from_at_date: " pada ",
+  },
+  response_box: {
+    login_to_respond: "Masuk untuk membalas!",
+    click_to_respond: "Klik untuk membuka panel media gambar",
+    open_painter: "Buka media gambar",
+    reply: "Balas!"
+  },
+  settings: {
+    category_general: "Umum",
+    general_language: "Bahasa",
+
+    category_sensitive: "Media Sensitif",
+    sensitive_blur_nsfw: "Kaburkan postingan NSFW",
+    sensitive_hide_nsfw: "Jangan tunjukkan postingan NSFW"
+  }
+};
