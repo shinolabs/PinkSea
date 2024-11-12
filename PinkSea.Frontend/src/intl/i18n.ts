e1079cd7fabcd8f25c5ca8e5eff35ef1d5b8b765
Verti <hello@vertigris.net>
2024-11-12 18:33:24 +0100

    Add German Translation
    Adds German translation to PinkSea

--- PinkSea.Frontend/src/intl/i18n.ts
+++ PinkSea.Frontend/src/intl/i18n.ts
@@ -2,6 +2,7 @@ import En from '@/intl/translations/en'
 import Id from '@/intl/translations/id'
 import Pl from '@/intl/translations/pl'
 import Ja from '@/intl/translations/ja'
+import De from '@/intl/translations/de'
 
 export default {
   en: {
@@ -15,5 +16,8 @@ export default {
   },
   ja: {
     translation: Ja
+  },
+  de: {
+    translation: De
   }
 };
