5b6841dfffe93266a1b129eb1824968907bd2540
purifetchi <0xlunaric@gmail.com>
2024-11-16 16:58:45 +0100

    feat: more cleanup for the begin login procedure.

--- PinkSea/Xrpc/BeginLoginFlowProcedure.cs
+++ PinkSea/Xrpc/BeginLoginFlowProcedure.cs
@@ -11,36 +11,53 @@ namespace PinkSea.Xrpc;
 [Xrpc("com.shinolabs.pinksea.beginLoginFlow")]
 public class BeginLoginFlowProcedure(
     IAtProtoOAuthClient oAuthClient,
-    IAtProtoAuthorizationService atProtoAuthorizationService)
+    IAtProtoAuthorizationService atProtoAuthorizationService,
+    ILogger<BeginLoginFlowProcedure> logger)
     : IXrpcProcedure<BeginLoginFlowProcedureRequest, BeginLoginFlowProcedureResponse>
 {
     /// <inheritdoc />
     public async Task<BeginLoginFlowProcedureResponse?> Handle(BeginLoginFlowProcedureRequest request)
     {
-        var normalizedHandle = request.Handle.TrimStart('@')
-            .ToLower();
-
-        // If we don't have a domain, that is, we don't have a '.' in the name
-        // let's just assume '.bsky.social' at the end.
-        // Usually people with custom domains don't do that.
-        if (!normalizedHandle.Contains('.'))
-            normalizedHandle += ".bsky.social";
-
-        if (request.Password is { Length: > 0 } )
+        try
         {
-            // Log in via a password.
-            var authorized = await atProtoAuthorizationService.LoginWithPassword(normalizedHandle, request.Password);
-            return !authorized.IsError
-                ? new BeginLoginFlowProcedureResponse { Redirect = $"{request.RedirectUrl}?code={authorized.Value}" }
-                : new BeginLoginFlowProcedureResponse { FailureReason = $"Unable to create a password session: {authorized.Error}" };
+            var normalizedHandle = request.Handle.TrimStart('@')
+                .ToLower();
+
+            // If we don't have a domain, that is, we don't have a '.' in the name
+            // let's just assume '.bsky.social' at the end.
+            // Usually people with custom domains don't do that.
+            if (!normalizedHandle.Contains('.'))
+                normalizedHandle += ".bsky.social";
+
+            if (request.Password is { Length: > 0 })
+            {
+                // Log in via a password.
+                var authorized =
+                    await atProtoAuthorizationService.LoginWithPassword(normalizedHandle, request.Password);
+                return !authorized.IsError
+                    ? new BeginLoginFlowProcedureResponse
+                        { Redirect = $"{request.RedirectUrl}?code={authorized.Value}" }
+                    : new BeginLoginFlowProcedureResponse
+                        { FailureReason = authorized.Error };
+            }
+
+            var authorizationServer = await oAuthClient.BeginOAuthFlow(
+                normalizedHandle,
+                request.RedirectUrl);
+
+            return authorizationServer.IsError
+                ? new BeginLoginFlowProcedureResponse
+                    { FailureReason = authorizationServer.Error }
+                : new BeginLoginFlowProcedureResponse { Redirect = authorizationServer.Value };
         }
-        
-        var authorizationServer = await oAuthClient.BeginOAuthFlow(
-            normalizedHandle,
-            request.RedirectUrl);
-        
-        return authorizationServer.IsError 
-            ? new BeginLoginFlowProcedureResponse { FailureReason = $"Unable to begin OAuth2 login flow: {authorizationServer.Error}" }
-            : new BeginLoginFlowProcedureResponse { Redirect = authorizationServer.Value };
+        catch (Exception e)
+        {
+            logger.LogError($"Failed to log-in for {request.Handle}", e);
+
+            return new BeginLoginFlowProcedureResponse
+            {
+                FailureReason = $"Server encountered an exception while performing your login: {e.Message}. Are you sure your handle is of the format @name.bsky.social?"
+            };
+        }
     }
 }
