8aba836a506e12b0d62d2492d2b995773194ddb5
purifetchi <0xlunaric@gmail.com>
2024-12-07 00:02:11 +0100

    experiment

---
+++ PinkSea.Gateway/Program.cs
@@ -0,0 +1,29 @@
+using PinkSea.Gateway;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapGet("/{did}/oekaki/{rid}", async (string did, string rid) =>
+{
+    var httpClient = new HttpClient();
+    var resp = await httpClient.GetFromJsonAsync<OekakiResponse>($"https://api.pinksea.art/xrpc/com.shinolabs.pinksea.getOekaki?did={did}&rkey={rid}");
+
+    var oembed = $@"
+<meta property=""og:title"" content=""PinkSea"" />
+<meta property=""og:type"" content=""website"" />
+<meta property=""og:url"" content=""https://pinksea.art/{did}/oekaki/{rid}"" />
+<meta property=""og:image"" content=""{resp!.Parent.ImageLink}"" />
+<meta property=""og:description"" content=""{resp!.Parent.Alt}"" />
+<meta name=""theme-color"" content=""#FF0000"">
+<meta name=""twitter:card"" content=""summary_large_image"">
+";
+
+    var file = File.ReadAllText($"./wwwroot/index.html")
+        .Replace("<!-- META -->", oembed);
+
+    return Results.Text(file, contentType: "text/html");
+});
+app.MapFallbackToFile("index.html");
+
+app.Run();
