4a5e368e56119112eb81f344f59c6e213de8f22f
purifetchi <0xlunaric@gmail.com>
2025-01-16 21:42:30 +0100

    chore: add the bluesky crosspost migrations.

---
+++ PinkSea/Migrations/20250116203026_Add the Bluesky crosspost tid field.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PinkSea.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddtheBlueskycrossposttidfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "BlueskyCrosspostRecordTid",
+                table: "Oekaki",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BlueskyCrosspostRecordTid",
+                table: "Oekaki");
+        }
+    }
+}
