023623b771805efc8074ed99149368d79a4ad2d0
purifetchi <0xlunaric@gmail.com>
2025-02-08 16:54:16 +0100

    feat: update translations from weblate.

--- PinkSea.Frontend/src/intl/i18n.ts
+++ PinkSea.Frontend/src/intl/i18n.ts
@@ -9,6 +9,9 @@ import Sv from '@/intl/translations/sv.json'
 import Es from '@/intl/translations/es.json'
 import Bg from '@/intl/translations/bg.json'
 import ZhCN from '@/intl/translations/zh-CN.json'
+import He from '@/intl/translations/he.json'
+import Nl from '@/intl/translations/nl.json'
+import PtBR from '@/intl/translations/pt-BR.json'
 
 export default {
   en: {
@@ -54,5 +57,17 @@ export default {
   "zh-CN": {
     name: '简体中文',
     translation: ZhCN,
+  },
+  nl: {
+    name: 'Nederlands',
+    translation: Nl,
+  },
+  "pt-BR": {
+    name: 'Português (Brasil)',
+    translation: PtBR,
+  },
+  he: {
+    name: 'עברית',
+    translation: He,
   },
 }
