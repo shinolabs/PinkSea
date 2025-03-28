a03eabfa592ac3ae67123a8a82f18cb2fb7b6f02
naomiEve <0xlunaric@gmail.com>
2025-03-28 15:38:38 +0100

    refactor: better error logging for XRPC handlers.

--- PinkSea.AtProto.Server/Xrpc/DefaultXrpcHandler.cs
+++ PinkSea.AtProto.Server/Xrpc/DefaultXrpcHandler.cs
@@ -1,5 +1,6 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using PinkSea.AtProto.Shared.Models;
 using PinkSea.AtProto.Shared.Xrpc;
 
 namespace PinkSea.AtProto.Server.Xrpc;
@@ -17,20 +18,41 @@ public class DefaultXrpcHandler(
     {
         var implementationType = XrpcTypeResolver.GetTypeMappingFor(nsid);
         if (implementationType is null)
-            return null;
+        {
+            return XrpcErrorOr<object>.Fail(
+                "NotFound",
+                $"Did not find a handler for {nsid}",
+                404);
+        }
         
         var service = serviceProvider.GetService(implementationType.HandlerType);
         if (service is null)
-            return null;
+        {
+            return XrpcErrorOr<object>.Fail(
+                "NoHandler",
+                $"No handler was registered for {nsid}.",
+                500);
+        }
 
         var requestObject = await GetObjectForType(implementationType, context);
-        if (requestObject is null)
-            return null;
+        if (requestObject.IsError)
+        {
+            return XrpcErrorOr<object>.Fail(
+                "FailedDeserialization",
+                requestObject.Error!);
+        }
+
+        if (requestObject.Value is null)
+        {
+            return XrpcErrorOr<object>.Fail(
+                "FailedDeserialization",
+                "An unknown error has occurred while deserializing the call parameters.");
+        }
 
         var method = service.GetType()
             .GetMethod("Handle")!;
         
-        var task = (Task)method.Invoke(service, [requestObject])!;
+        var task = (Task)method.Invoke(service, [requestObject.Value])!;
         await task.ConfigureAwait(false);
 
         var resultProperty = task.GetType()
@@ -46,30 +68,40 @@ public class DefaultXrpcHandler(
     /// <param name="typeMapping">The XRPC type mapping.</param>
     /// <param name="context">The HTTP context.</param>
     /// <returns>The deserialized object.</returns>
-    private static async Task<object?> GetObjectForType(
+    private static async Task<ErrorOr<object?>> GetObjectForType(
         XrpcTypeMapping typeMapping,
         HttpContext context)
     {
-        // If this is a procedure, we just have to parse JSON. Nice!
-        if (!typeMapping.IsQuery)
+        try
         {
-            var json = await JsonSerializer.DeserializeAsync(
-                context.Request.Body,
-                typeMapping.RequestType);
+            // If this is a procedure, we just have to parse JSON. Nice!
+            if (!typeMapping.IsQuery)
+            {
+                var json = await JsonSerializer.DeserializeAsync(
+                    context.Request.Body,
+                    typeMapping.RequestType);
 
-            return json;
-        }
+                return ErrorOr<object?>.Ok(json);
+            }
         
-        // Dirty hack :3
-        var queryDict = context
-            .Request
-            .Query
-            .ToDictionary(k => k.Key, v => v.Value.ToString());
+            // Dirty hack :3
+            var queryDict = context
+                .Request
+                .Query
+                .ToDictionary(k => k.Key, v => v.Value.ToString());
 
-        var queryJson = JsonSerializer.Serialize(queryDict);
-        return JsonSerializer.Deserialize(queryJson, typeMapping.RequestType, new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            var queryJson = JsonSerializer.Serialize(queryDict);
+            return ErrorOr<object?>.Ok(JsonSerializer.Deserialize(
+                queryJson,
+                typeMapping.RequestType,
+                new JsonSerializerOptions(JsonSerializerDefaults.Web)
+            {
+                NumberHandling = JsonNumberHandling.AllowReadingFromString
+            }));
+        }
+        catch (JsonException e)
         {
-            NumberHandling = JsonNumberHandling.AllowReadingFromString
-        });
+            return ErrorOr<object?>.Fail($"Failed deserializing parameters for XRPC call: {e.Message}");
+        }
     }
 }
