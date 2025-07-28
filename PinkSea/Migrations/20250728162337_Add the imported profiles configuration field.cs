9af896e944d25879dac99ec8e7e5d32a801c41bb
purifetchi <0xlunaric@gmail.com>
2025-07-28 18:32:54 +0200

    feat: implement profile backfill for existing databases.

---
+++ PinkSea/Migrations/20250728162337_Add the imported profiles configuration field.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PinkSea.Migrations
+{
+    /// <inheritdoc />
+    public partial class Addtheimportedprofilesconfigurationfield : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ImportedProfiles",
+                table: "Configuration",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImportedProfiles",
+                table: "Configuration");
+        }
+    }
+}
