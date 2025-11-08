49fbc7a994619c29e1224848a44687eaef290f63
mcddx330 <monobeat.030+github@gmail.com>
2025-11-08 12:07:26 +0900

    rebase master.

---
+++ PinkSea.Frontend/src/intl/translations/ja.ts
@@ -0,0 +1,49 @@
+export default {
+  sidebar: {
+    title: "PinkSea",
+    tag: "oekaki BBS",
+    shinolabs: "shinonome laboratories プロジェクト"
+  },
+  menu: {
+    greeting: "@{{name}} さん、こんにちは！",
+    invitation: "ログインしてお絵かきしよう！",
+    input_placeholder: "あなたのBluesky ID （例： @あなたのID.bsky.social）",
+    atp_login: "ログイン",
+    my_oekaki: "投稿記録",
+    recent: "最近の投稿",
+    settings: "設定",
+    logout: "ログアウト",
+    create_something: "お絵かきする"
+  },
+  breadcrumb: {
+    recent: "最近の投稿",
+    painter: "作者",
+    settings: "あなたのお絵かき設定",
+    user_profile: "{{handle}} のプロフィール",
+    user_post: "{{handle}} の投稿",
+    tagged: "#{{tag}} タグの投稿一覧"
+  },
+  timeline: {
+    by_before_handle: "作者 ",
+    by_after_handle: "",
+  },
+  post: {
+    response_from_before_handle: "次の投稿への返信 ",
+    response_from_after_handle: "",
+    response_from_at_date: " の ",
+  },
+  response_box: {
+    login_to_respond: "ログインして返信！",
+    click_to_respond: "クリックしてお絵かき画面を表示",
+    open_painter: "お絵かき画面を表示",
+    reply: "返信！"
+  },
+  settings: {
+    category_general: "総合",
+    general_language: "言語",
+
+    category_sensitive: "センシティブな投稿",
+    sensitive_blur_nsfw: "NSFWな投稿を「ぼかして」表示",
+    sensitive_hide_nsfw: "NSFWな投稿を表示しない"
+  }
+};
