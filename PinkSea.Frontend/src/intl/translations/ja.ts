75b056cec5c5d63e68ff3cdd929c251aaca45e41
mcddx330 <mcddx330@users.noreply.github.com>
2025-11-10 20:21:06 +0900

    Update Japanese localization. (#74)
    * rebase master.
    
    * rebase master.
    
    * 時->際
    
    * fix translate.
    
    * update Japanese localization.

---
+++ PinkSea.Frontend/src/intl/translations/ja.ts
@@ -0,0 +1,61 @@
+export default {
+  sidebar: {
+    title: "PinkSea",
+    tag: "oekaki BBS",
+    shinolabs: "プロジェクト shinonome laboratories"
+  },
+  menu: {
+    greeting: "@{{name}} さん、こんにちは！",
+    invitation: "ログインしてお絵かきしよう！",
+    input_placeholder: "@あなたのID.bsky.social",
+    atp_login: "ログイン",
+    my_oekaki: "投稿記録",
+    recent: "最近の投稿",
+    settings: "設定",
+    logout: "ログアウト",
+    create_something: "お絵かきする"
+  },
+  breadcrumb: {
+    recent: "最近の投稿",
+    painter: "作者",
+    settings: "PinkSeaの設定",
+    user_profile: "{{handle}} のプロフィール",
+    user_post: "{{handle}} の投稿",
+    tagged: "#{{tag}} タグの投稿一覧"
+  },
+  timeline: {
+    by_before_handle: "作者 ",
+    by_after_handle: "",
+  },
+  post: {
+    response_from_before_handle: "この投稿への返信 ",
+    response_from_after_handle: "",
+    response_from_at_date: " の ",
+  },
+  response_box: {
+    login_to_respond: "ログインして返信！",
+    click_to_respond: "クリックしてお絵かき画面を表示",
+    open_painter: "お絵かき画面を表示",
+    reply: "返信！",
+    cancel: "キャンセル"
+  },
+  settings: {
+    category_general: "総合",
+    general_language: "言語",
+
+    category_sensitive: "センシティブな投稿",
+    sensitive_blur_nsfw: "NSFWな投稿を「ぼかして」表示",
+    sensitive_hide_nsfw: "NSFWな投稿を表示しない"
+  },
+  painter: {
+    do_you_want_to_restore: "直前の作品をアップロードする際にエラーが発生しました。保存された作品を復元しますか？",
+    could_not_send_post: "直前の作品をアップロードする際にエラーが発生しました。作品はブラウザ内に保存されているため、後でもう一度投稿をお試しください。",
+    add_a_description: "画像の説明文をここに記入",
+    tag: "タグ",
+    upload: "投稿！",
+    crosspost_to_bluesky: "BlueSkyにも投稿する"
+  },
+  profile: {
+    bluesky_profile: "Bluesky上のプロフィール"
+  }
+};
